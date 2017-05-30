9f49a51d8c9f2be8c76da68a02f96cdd619fd65d
Zhi Wen <zwen120@outlook.com>
2017-05-30 18:23:39 +0800

    Update Get_start.m
    Using both height data and acceleration data to determine the start point.

--- Get_start.m
+++ Get_start.m
@@ -1,15 +1,54 @@
 function st_index = Get_start( acce, sam_rate )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-threshold=0.94;  % Thresold for determining start point
-si=size(acce,1);
-acce_st=Acce_index(acce,sam_rate,threshold);
-dece_st=Dece_index(acce,sam_rate,threshold);
-% if(acce_st==dece_st)
-%     acce_st=Acce_index(acce,sam_rate);
-%     dece_st=Dece_index(acce,sam_rate);
-% end
-st_index=min([acce_st dece_st]);
+% Set duration
+duration=4;
 
+% Set threshold
+threshold_cos=0.93;
+threshold_h=0.07;
+
+% Get vector length
+si=size(raw_data,1);
+
+% Get height data
+hei=raw_data(:,end);
+hei=hei-hei(1,1);
+
+% Get acceleration data
+acce=9.8.*raw_data(:,2:4);
+    
+% Cancle noise
+acce(:,3)=acce(:,3)-acce(1,3);
+
+acce=acce';
+Max=max(acce(3,:));
+t=1:duration*sam_rate;
+test_acce=-t.*(t-duration*sam_rate-1);
+Max_t=max(test_acce);
+test_acce=(Max./Max_t).*test_acce;
+Min=min(acce(3,:));
+t=1:duration*sam_rate;
+test_dece=t.*(t-duration*sam_rate-1);
+Min_t=min(test_dece);
+test_dece=(Min./Min_t).*test_dece;
+
+st_index=1;
+
+for i=1:1:si(1)-sam_rate*duration
+    if(hei(i)>threshold_h)
+        cos_angle=dot(test_acce,acce(3,i:i+sam_rate*duration-1))/(norm(acce(3,i:i+sam_rate*duration-1))*norm(test_acce));
+        if(cos_angle>threshold_cos)
+            st_index=i;
+            break
+        end
+    elseif(hei(i)<-threshold_h)
+        cos_angle=dot(test_dece,acce(3,i:i+sam_rate*duration-1))/(norm(acce(3,i:i+sam_rate*duration-1))*norm(test_dece));
+        if(cos_angle>threshold_cos)
+            st_index=i;
+            break
+        end
+    end
+end
 end
 
