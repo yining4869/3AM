da272fb7e697c2d436d505d42c90c107334e5b39
Zhi Wen <zwen120@outlook.com>
2017-05-30 18:26:10 +0800

    Update Comparator_frequency.m
    To fit with Get_start.

--- Comparator_frequency.m
+++ Comparator_frequency.m
@@ -100,7 +100,7 @@ while(index)
 %     acce(:,2)=acce(:,2)-mean(acce(:,2));
     acce(:,3)=acce(:,3)-mean(acce(:,3));
     
-    st_index=Get_start(acce,sam_rate);
+    st_index=Get_start(raw_data,sam_rate);
     acce=acce(st_index:end,:);
     si=size(acce,1);
     
