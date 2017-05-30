5c8ca179689148048222d6bd3003875184360568
Zhi Wen <zwen120@outlook.com>
2017-05-30 18:29:34 +0800

    Update Comparator.m

--- Comparator.m
+++ Comparator.m
@@ -94,9 +94,9 @@ while(index)
     acce=g.*raw_data(:,2:4);
     
     % Cancle noise
-    acce(:,1)=acce(:,1)-mean(acce(:,1));
-    acce(:,2)=acce(:,2)-mean(acce(:,2));
-    acce(:,3)=acce(:,3)-mean(acce(:,3));
+    % acce(:,1)=acce(:,1)-mean(acce(:,1));
+    % acce(:,2)=acce(:,2)-mean(acce(:,2));
+    acce(:,3)=acce(:,3)-acce(1,3);
     
     st_index=Get_start(raw_data,sam_rate);
     acce=acce(st_index:end,:);
