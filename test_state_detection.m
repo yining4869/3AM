12278b6b5c9baab4f94b072fb5d01bcebc64748f
Zhi Wen <zwen120@outlook.com>
2017-07-29 11:31:16 +0800

    Enable detection of states during multiple process
    Updated version can detect states through multiple motion processes, rather than one process that contains only one acceleration and one deceleration.

--- test_state_detection.m
+++ test_state_detection.m
@@ -11,7 +11,7 @@ MOV_DOWN=5;
 ACCE_TOSTILL=6;
 DECE_TOSTILL=7;
 
-[acce_st, dece_st, steady_st, still_back, acce_tostill, dece_tostill]=deal(0);
+[acce_st, dece_st, steady_st, still_back, acce_tostill, dece_tostill]=deal([]);
 
 threshold=0.1;
 
@@ -31,75 +31,82 @@ acce(:,3)=acce(:,3)-acce(1,3);
 % Time
 x=(0:1/sam_rate:(si-1)/sam_rate)';
 
-
-time_acce=1;
-time_dece=1;
-
 state=STILL;
 
-for i=1:si
-    switch state
-    case STILL
-        if i==acce_index
-            state=ACCE;
-            acce_st=i/sam_rate;
-        elseif i==dece_index
-            state=DECE;
-            dece_st=i/sam_rate;
-        end
-    case ACCE
-        if (i>=acce_index+sam_rate*duration)&&(acce(i,3)<=threshold)
-            state=MOV_UP;
-            steady_st=i/sam_rate;
-        end
-    case ACCE_TOSTILL
-        if (i>=acce_index+sam_rate*duration)&&(acce(i,3)<=threshold)
-            state=STILL;
-            still_back=i/sam_rate;
-        end
-    case MOV_UP
-        if i==dece_index
-            state=DECE_TOSTILL;
-            dece_tostill=i/sam_rate;
-        end
-    case MOV_DOWN
-        if i==acce_index
-            state=ACCE_TOSTILL;
-            acce_tostill=i/sam_rate;
-        end
-    case DECE
-        if (i>=dece_index+sam_rate*duration)&&(acce(i,3)>=-threshold)
-            state=MOV_DOWN;
-            steady_st=i/sam_rate;
-        end
-    case DECE_TOSTILL
-        if (i>=dece_index+sam_rate*duration)&&(acce(i,3)>=-threshold)
-            state=STILL;
-            still_back=i/sam_rate;
-        end
-    end
-end
+n_acce=1;
+n_dece=1;
 
 plot(x,acce(:,3));
 title('Acceleration Az');
 ylabel('Az m/s^2');
 xlabel('t /s');
+grid on;zoom on;
+
+
+for i=1:si
+    switch state
+    case STILL
+        if (n_acce<=size(acce_index,1))&&(n_dece<=size(dece_index,1))    
+            if i==acce_index(n_acce)
+                state=ACCE;
+                acce_st=[acce_st;i];
+                n_acce=n_acce+1;
+            elseif i==dece_index(n_dece)
+                state=DECE;
+                dece_st=[dece_st;i];
+                n_dece=n_dece+1;
+            end
+        end
+    case ACCE
+        if (i>=acce_index(n_acce-1)+sam_rate*duration)&&(acce(i,3)<=threshold)
+            state=MOV_UP;
+            steady_st=[steady_st;i];
+        end
+    case ACCE_TOSTILL
+        if (i>=acce_index(n_acce-1)+sam_rate*duration)&&(acce(i,3)<=threshold)
+            state=STILL;
+            still_back=[still_back;i];
+        end
+    case MOV_UP
+        if i==dece_index(n_dece)
+            state=DECE_TOSTILL;
+            dece_tostill=[dece_tostill;i];
+            n_dece=n_dece+1;
+        end
+    case MOV_DOWN
+        if i==acce_index(n_acce)
+            state=ACCE_TOSTILL;
+            acce_tostill=[acce_tostill;i];
+            n_acce=n_acce+1;
+        end
+    case DECE
+        if (i>=dece_index(n_dece-1)+sam_rate*duration)&&(acce(i,3)>=-threshold)
+            state=MOV_DOWN;
+            steady_st=[steady_st;i];
+        end
+    case DECE_TOSTILL
+        if (i>=dece_index(n_dece-1)+sam_rate*duration)&&(acce(i,3)>=-threshold)
+            state=STILL;
+            still_back=[still_back;i];
+        end
+    end
+end
 
 if acce_st~=0
-    text(acce_st,-0.01,'\uparrow start moving up');
+    text(acce_st./sam_rate,-0.01.*ones(size(acce_st,1),1),'\uparrow start moving up');
 end
 if dece_st~=0
-    text(dece_st,0.01,'\downarrow start moving down');
+    text(dece_st./sam_rate,0.01.*ones(size(dece_st,1),1),'\downarrow start moving down');
 end
 if steady_st~=0
-    text(steady_st,-0.01,'\uparrow begin steady movement');
+    text(steady_st./sam_rate,-0.02.*ones(size(steady_st,1),1),'\uparrow begin steady movement');
 end
 if still_back~=0
-    text(still_back,-0.01,'\uparrow back to stillness');
+    text(still_back./sam_rate,-0.02.*ones(size(still_back,1),1),'\uparrow back to stillness');
 end
 if dece_tostill~=0
-    text(dece_tostill,0.01,'\downarrow decelerate to stillness');
+    text(dece_tostill./sam_rate,0.03.*ones(size(dece_tostill,1),1),'\downarrow decelerate to stillness');
 end
 if acce_tostill~=0
-    text(acce_tostill,-0.01,'\uparrow accelerate to stillness');
+    text(acce_tostill./sam_rate,-0.03.*ones(size(acce_tostill,1),1),'\uparrow accelerate to stillness');
 end
